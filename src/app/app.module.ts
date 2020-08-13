d124788adb0713bbf4a7131a93daca8c7ee9c4dc
unknown <yunior.diaz@emqu.net>
2020-08-13 19:14:05 -0430

    feat: ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './main/navigation/navigation.component';
+import { HomeComponent } from './main/home/home.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { AboutComponent } from './main/about/about.component';
+
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService
+} from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang("en");
+  }
+}
